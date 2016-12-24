6ba539e6d30aeb3a47d73c539cb061754b920149
Yashar <yashar1@users.noreply.github.com>
2016-12-24 13:36:39 -0800

    Running Count JS

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,28 @@
+// User Clicks 10-A The Running Count Goes Down by 1
+
+$('.decreaseCount').click(function() {
+  var count = parseInt($('#trueCount').text());
+  $('#trueCount').text(count-1);
+});
+
+// User Clicks 2-6 The Running Count Goes Up by 1
+
+$('.increaseCount').click(function() {
+  var count = parseInt($('#trueCount').text());
+  $('#trueCount').text(count+1);
+});
+
+// User Clicks 7-9 The Running Count Doesn't Change
+
+$('.noCount').click(function() {
+  var count = parseInt($('#trueCount').text());
+  $('#trueCount').text(count);
+});
+
+// Reset All Values to their Defaults
+
 function resetValues() {
-  $('#result').text('0');
+  $('#trueCount').text('0');
   $('#data').text('0.0');
   $('#bet').text('Bet 1x');
-  $("#select").val("selectDeck");
 };
