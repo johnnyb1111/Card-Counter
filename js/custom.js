f879801eec70fa4a64761c09fe331d836d83472f
Yashar <yashar1@users.noreply.github.com>
2016-12-24 02:36:24 -0800

    Button Linking

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 function resetValues() {
-    $('#result').text('0');
-    $('#data').text('0.0');
-    $('#bet').text('Bet 1x');
-    $("#select").val("selectDeck");
+  $('#result').text('0');
+  $('#data').text('0.0');
+  $('#bet').text('Bet 1x');
+  $("#select").val("selectDeck");
 };
