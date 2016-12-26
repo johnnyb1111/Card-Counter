cda08c8712f12c6b9e5871dd08e8026ec49f46de
Yashar <yashar1@users.noreply.github.com>
2016-12-26 01:42:05 -0800

    Modal and Help Styling

--- js/custom.js
+++ js/custom.js
@@ -6,49 +6,49 @@ var count = 0;
 // Get Deck Count from Segmented Controller
 
 function deckCount1() {
-	var deckCount = 1;
+  var deckCount = 1;
 };
 
 function deckCount2() {
-	var deckCount = 2;
+  var deckCount = 2;
 };
 
 function deckCount3() {
-	var deckCount = 3;
+  var deckCount = 3;
 };
 
 function deckCount4() {
-	var deckCount = 4;
+  var deckCount = 4;
 };
 
 function deckCount5() {
-	var deckCount = 5;
+  var deckCount = 5;
 };
 
 function deckCount6() {
-	var deckCount = 6;
+  var deckCount = 6;
 };
 
 function deckCount7() {
-	var deckCount = 7;
+  var deckCount = 7;
 };
 
 function deckCount8() {
-	var deckCount = 8;
+  var deckCount = 8;
 };
 
 // User Clicks 10-A The Running Count Goes Down by 1
 
 $('.decreaseCount').click(function() {
   var count = parseInt($('#runningCount').text());
-  $('#runningCount').text(count-1);
+  $('#runningCount').text(count - 1);
 });
 
 // User Clicks 2-6 The Running Count Goes Up by 1
 
 $('.increaseCount').click(function() {
   var count = parseInt($('#runningCount').text());
-  $('#runningCount').text(count+1);
+  $('#runningCount').text(count + 1);
 });
 
 // User Clicks 7-9 The Running Count Doesn't Change
