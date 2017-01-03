3ed46b622eb547bae0d0b264e64f273479458149
Yashar <yashar1@users.noreply.github.com>
2017-01-03 01:06:34 -0800

    JS Cleanup

--- js/custom.js
+++ js/custom.js
@@ -7,35 +7,35 @@ var count = 0;
 
 function deckCount1() {
   var deckCount = 1;
-};
+}
 
 function deckCount2() {
   var deckCount = 2;
-};
+}
 
 function deckCount3() {
   var deckCount = 3;
-};
+}
 
 function deckCount4() {
   var deckCount = 4;
-};
+}
 
 function deckCount5() {
   var deckCount = 5;
-};
+}
 
 function deckCount6() {
   var deckCount = 6;
-};
+}
 
 function deckCount7() {
   var deckCount = 7;
-};
+}
 
 function deckCount8() {
   var deckCount = 8;
-};
+}
 
 // User Clicks 10-A The Running Count Goes Down by 1
 
@@ -64,4 +64,4 @@ function resetValues() {
   $("#runningCount").text("0");
   $("#trueCount").text("0.0");
   $("#bet").text("Bet 1x");
-};
+}
