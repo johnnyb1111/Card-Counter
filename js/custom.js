47bdfe7c741a5b0780ef950fe0a7ad2435bdb6ad
Yashar <yashar1@users.noreply.github.com>
2017-01-03 00:59:35 -0800

    Markup Improvements

--- js/custom.js
+++ js/custom.js
@@ -39,29 +39,29 @@ function deckCount8() {
 
 // User Clicks 10-A The Running Count Goes Down by 1
 
-$('.decreaseCount').click(function() {
-  var count = parseInt($('#runningCount').text());
-  $('#runningCount').text(count - 1);
+$(".decreaseCount").click(function() {
+  var count = parseInt($("#runningCount").text());
+  $("#runningCount").text(count - 1);
 });
 
 // User Clicks 2-6 The Running Count Goes Up by 1
 
-$('.increaseCount').click(function() {
-  var count = parseInt($('#runningCount').text());
-  $('#runningCount').text(count + 1);
+$(".increaseCount").click(function() {
+  var count = parseInt($("#runningCount").text());
+  $("#runningCount").text(count + 1);
 });
 
-// User Clicks 7-9 The Running Count Doesn't Change
+// User Clicks 7-9 The Running Count Doesn"t Change
 
-$('.noCount').click(function() {
-  var count = parseInt($('#runningCount').text());
-  $('#runningCount').text(count);
+$(".noCount").click(function() {
+  var count = parseInt($("#runningCount").text());
+  $("#runningCount").text(count);
 });
 
 // Reset All Values to their Defaults
 
 function resetValues() {
-  $('#runningCount').text('0');
-  $('#trueCount').text('0.0');
-  $('#bet').text('Bet 1x');
+  $("#runningCount").text("0");
+  $("#trueCount").text("0.0");
+  $("#bet").text("Bet 1x");
 };
